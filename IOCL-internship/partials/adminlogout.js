9343f36c1c0e4f17a3e28772df837e28f890f6b9
Ekta <ektaverma@outlook.com>
2024-07-30 12:16:36 +0530

    Added new files

--- IOCL-internship/partials/adminlogout.js
+++
@@ -1,7 +0,0 @@
-
-function confirmLogout(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    if (confirm("Are you sure you want to log out?")) {
-        window.location.href = 'index.html'; // Redirect to logout.php if confirmed
-    }
-}
