fbef92927378dcdf41a39b1a6c4f1a77e6859447
Ekta Verma <164794593+EktaVerma24@users.noreply.github.com>
2024-07-30 12:36:17 +0530

    Update connect.php

--- partials/connect.php
+++ partials/connect.php
@@ -1,9 +1,9 @@
 <?php
 $host = "localhost";
 $username = "root";
-$password = null;  // assuming no password
+$password = "1234321";  // Ensure this matches the MYSQL_ROOT_PASSWORD in Docker Compose
 $database = "iocl";
-$port = 3307;  // specify your port number here
+$port = 3307;  // Use the port you've mapped
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database, $port);
@@ -13,3 +13,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
