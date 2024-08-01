c4b0287539f21c1c02f77db17f52075cfc980a04
Ekta <ektaverma@outlook.com>
2024-08-01 16:10:35 +0530

    add env

--- partials/connect.php
+++ partials/connect.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST') ?: 'localhost';  // Fallback to localhost if not set
 $username = getenv('DB_USER') ?: 'root';   // Fallback to root if not set
 $password = getenv('DB_PASS') ?: '';       // Fallback to empty string if not set
 $database = getenv('DB_NAME') ?: 'iocl';   // Fallback to iocl if not set
-$port = getenv('DB_PORT') ?: 3306;         // Fallback to 3306 if not set
+$port = getenv('DB_PORT') ?: 3307;         // Fallback to 3306 if not set
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $database, $port);
